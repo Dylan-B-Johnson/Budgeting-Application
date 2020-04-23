23608d9d0e475acad3af4a1b95572f4655f65157
maxbear123 <45766928+maxbear123@users.noreply.github.com>
2020-04-23 11:41:17 -0400

    Add files via upload
    - fixed circular import
    - made remove and add buttons work perfectly (can do both with existing stocks and in batches and will never display two identical stocks separately)

--- portfolio_viewer.py
+++ portfolio_viewer.py
@@ -3,12 +3,13 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter_toolbox import Dlabel
 from yahoo_fin import stock_info as si
-from budgeting_app import truncate
+import budgeting_app as ba
 import numpy as np
 import datetime
 import pickle
 
-def init():
+# def init():
+if __name__ == "__main__":
 	global stock_view
 	global delete_port_attemps
 	global stocks
@@ -21,41 +22,18 @@ def init():
 	delete_port_attemps=0
 	stocks=[[]]
 
-
-	#builds menu bars
-	menu = Menu(window2)
-	new_item = Menu(menu)
-	new_item.add_command(label='Save Portfolio',command=lambda: save_portfolio())
-	new_item.add_command(label='Load Portfolio',command=lambda: load_portfolio())
-	menu.add_cascade(label='File', menu=new_item)
-	window2.config(menu=menu)
-
-	#builds GUI
-	stock_directions = Label(window2,text=('Add or remove stocks from your portfolio by typing the stock\'s ticker, followed by a space and the number of shares to be added/removed.\n' +
-		'To enter multiple at once, simply add a comma and one space between them.\n Example: "AMZN 4, AAPL 5"'))
-	stock_directions.grid(column=2, row=1)
-	stock_entry_box = Entry(window2,width=100)
-	stock_entry_box.grid(column=2, row=2)
-	add_stock = Button(window2,text='Add Stocks', command= lambda: parse_stocks(stock_entry_box.get(),'add'))
-	add_stock.grid(column=1, row=3)
-	remove_stock = Button(window2,text='Remove Stocks', command= lambda: parse_stocks(stock_entry_box.get(),'remove'))
-	remove_stock.grid(column=0, row=3)
-	remove_stock = Button(window2,text='Delete Portfolio', command= lambda: delete_portfolio())
-	remove_stock.grid(column=0, row=4)
-	update= Button(window2,text='Update Prices', command= lambda: update_stocks())
-	update.grid(column=1, row=4)
-	window2.mainloop()
-
 def parse_stocks(text,mode):
 	global stocks
+	print(stocks)
 	if mode=='remove': remove_stocks=[[]]
 	start=True
 	something_went_wrong=False
 	last_char_comma=False
 	if len(stocks[0])==2: i2=len(stocks)
 	else: i2=0
+	i4=0
 	i3=1
-	try:                    
+	try:                  
 		for i in text:
 			if len(stocks[0])==2 and start:
 				if mode=='add':
@@ -69,24 +47,28 @@ def parse_stocks(text,mode):
 					stocks[i2].append(int(stock_num))
 					stocks.append([])
 				else:
-					remove_stocks[i2].append(int(stock_num))
+					remove_stocks[i4].append(int(stock_num))
 					remove_stocks.append([])
 				last_char_comma=True
 				i2+=1
+				i4+=1
 			elif i.isalpha(): stock+=i
 			elif i==' ': 
 				if last_char_comma==True:
 					last_char_comma=False
 				else: 
 					if mode=='add': stocks[i2].append((stock.lower()))
-					else: remove_stocks[i2].append((stock.lower()))
+					else: 
+						print('stocks: '+str())
+						print('remove: '+str(remove_stocks))
+						remove_stocks[i4].append((stock.lower()))
 					stock=''
 					stock_num=''
 			elif i.isnumeric():
 				stock_num+=i
 				if i3==len(text): 
 					if mode=='add': stocks[i2].append(int(stock_num))
-					else: remove_stocks[i2].append(int(stock_num))
+					else: remove_stocks[i4].append(int(stock_num))
 			else: something_went_wrong=True
 			i3+=1
 
@@ -111,6 +93,24 @@ def parse_stocks(text,mode):
 					for i in del_list: del stocks[i]
 				except: messagebox.showinfo('Deleting Error','Something went wrong. Check for typos, or make sure you actually have the stock you think you have.')
 
+			#checking for duplicates and summing them
+			if mode=='add':
+				seen=[]
+				del_list=[]
+				for i in stocks:
+					if len(seen)>=1:
+						i2=0
+						for i_seen in seen:
+							if i_seen[0]==i[0]:
+								i[1]+=i_seen[1]
+								del_list.append(i2)
+							i2+=1
+					seen.append(i)
+
+				if len(del_list) >= 1:
+					for i in del_list: 
+						stocks.remove(seen[i])				
+
 			#error checking + printing stocks
 			total=0
 			i2=0
@@ -124,11 +124,11 @@ def parse_stocks(text,mode):
 			 			'Your other stocks were still entered, so entering them again will double the number of stocks you have.')
 			 		del_list.append(i2)
 			 	# else: 
-			 	# 	print('\n'+i[0].upper()+': $'+truncate(price,2)+' x '+str(i[1])+' = $'+truncate((i[1]*price),2))
+			 	# 	print('\n'+i[0].upper()+': $'+ba.truncate(price,2)+' x '+str(i[1])+' = $'+ba.truncate((i[1]*price),2))
 			 	# 	total+=i[1]*price
 			 	i2+=1
 			for i in del_list: del stocks[i]
-			# print('\nTotal: $'+truncate(total,2))
+			# print('\nTotal: $'+ba.truncate(total,2))
 			update_stocks()
 	except:
 		messagebox.showinfo('Error','Something went wrong. Check for typos, and make sure that you typed a space followed by a number after each ticker. If any'+
@@ -146,12 +146,12 @@ def update_stocks():
 			print('\n-------------------------------Portfolio-------------------------------\nPrices as of '+now.strftime("%m-%d-%Y %H:%M:%S"))
 			for i in stocks:
 				price=si.get_live_price(i[0])
-				string=('\n'+i[0].upper()+': $'+truncate(price,2)+' x '+str(i[1])+' = $'+truncate((i[1]*price),2))
+				string=('\n'+i[0].upper()+': $'+ba.truncate(price,2)+' x '+str(i[1])+' = $'+ba.truncate((i[1]*price),2))
 				print(string)
 				stock_print+='\n'+string
 				total+=i[1]*price
-			print('\nTotal: $'+truncate(total,2))
-			stock_print+=('\n\nTotal: $'+truncate(total,2))
+			print('\nTotal: $'+ba.truncate(total,2))
+			stock_print+=('\n\nTotal: $'+ba.truncate(total,2))
 			stock_view.configure(stock_print)
 		except:
 			messagebox.showinfo('Error','Something went wrong. Check for typos, and make sure that you typed a space followed by a number after each ticker. If any'+
@@ -183,3 +183,28 @@ def delete_portfolio():
 		stock_view.configure('')
 		save_portfolio()
 		delete_port_attemps=0
+
+if __name__ == "__main__":
+	#builds menu bars
+	menu = Menu(window2)
+	new_item = Menu(menu)
+	new_item.add_command(label='Save Portfolio',command=lambda: save_portfolio())
+	new_item.add_command(label='Load Portfolio',command=lambda: load_portfolio())
+	menu.add_cascade(label='File', menu=new_item)
+	window2.config(menu=menu)
+
+	#builds GUI
+	stock_directions = Label(window2,text=('Add or remove stocks from your portfolio by typing the stock\'s ticker, followed by a space and the number of shares to be added/removed.\n' +
+		'To enter multiple at once, simply add a comma and one space between them.\n Example: "AMZN 4, AAPL 5"'))
+	stock_directions.grid(column=2, row=1)
+	stock_entry_box = Entry(window2,width=100)
+	stock_entry_box.grid(column=2, row=2)
+	add_stock = Button(window2,text='Add Stocks', command= lambda: parse_stocks(stock_entry_box.get(),'add'))
+	add_stock.grid(column=1, row=3)
+	remove_stock = Button(window2,text='Remove Stocks', command= lambda: parse_stocks(stock_entry_box.get(),'remove'))
+	remove_stock.grid(column=0, row=3)
+	remove_stock = Button(window2,text='Delete Portfolio', command= lambda: delete_portfolio())
+	remove_stock.grid(column=0, row=4)
+	update= Button(window2,text='Update Prices', command= lambda: update_stocks())
+	update.grid(column=1, row=4)
+	window2.mainloop()
