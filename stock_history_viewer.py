cf45d07b0754fab28208a05b94771f03e0585989
maxbear123 <45766928+maxbear123@users.noreply.github.com>
2020-08-15 16:49:16 -0400

    Update stock_history_viewer.py

--- stock_history_viewer.py
+++ stock_history_viewer.py
@@ -1,3 +1,23 @@
+"""
+Copyright 2020 Dylan Johnson
+
+This file is part of Budgeting App.
+
+    Budgeting App is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Budgeting App is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Budgeting App.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 import tkinter as tk
 import budgeting_app as ba
 from tkinter_toolbox import bubble_options
